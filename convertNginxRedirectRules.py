6b2110e6a83d018a0fd3f5ddb74c14a69c6283df
SergioMeneses <sergioandresmeneses@gmail.com>
2018-06-30 16:43:16 -0500

    Included TXT files support and Regex level1
    Regex level1 will adjust the origin URL to the format

--- convertNginxRedirectRules.py
+++ convertNginxRedirectRules.py
@@ -7,29 +7,31 @@ import sys
 #Function to format the urls in the proper cases
 #Queries, HTML Trans and direct format supported.
 def fformat(oringUrl, destinyUrl):
+
+    #regex validations to make the script compatible with most formats of the origin URL
+    #Evaluates if the url has HTTP included
+    #HTTPS support soon
+    regex = r"^http://(.*)\/"
+    if re.search(regex, oringUrl):
+        oringUrl = re.sub(regex, "/", oringUrl)
+    #Evaluates if the url begings with / and add it in case of not including it
+    regex = r"/"
+    if re.match(regex, oringUrl) == None:
+        oringUrl = "/"+oringUrl
+    #####################################
+    #
     #Evaluates if the line is a query : ?
     if str(oringUrl.find("?")) != "-1":
-        print ("Query!!!")
         #Split the String to make up the special rule.
         OrigUrlBefore,OrigUrlAfter = oringUrl.split("?")
-        print ( "Before ?: " + OrigUrlBefore)
-        print ( "After ?: " + OrigUrlAfter)
-        subprocess.Popen("echo \" if ( \$query_string ~ '"+OrigUrlAfter+"' ) { rewrite '^/"+OrigUrlBefore+"/?$' "+str(destinyUrl)+"? permanent; } \" >> rules-formatted.inc", shell=True)
-        print("-------------Query-----------------")
+        subprocess.Popen("echo \" if ( \$query_string ~ '"+OrigUrlAfter+"' ) { rewrite '^"+OrigUrlBefore+"/?$' "+str(destinyUrl)+"? permanent; } \" >> rules-formatted.inc", shell=True)
         return
-    else:
-        print ("FAIL Query!!!")
     #Evaluates if the line is HTML Trash : ?
-    if str(oringUrl.find("%")) != "-1":
-        print ("HTML Trash!!!")
-        subprocess.Popen("echo \" if ( \$request_uri ~* '^/"+str(oringUrl)+"/?$' ) { rewrite . "+str(destinyUrl)+" permanent; } \" >> rules-formatted.inc", shell=True)
-        print("-------------HTML-----------------")
+    elif str(oringUrl.find("%")) != "-1":
+        subprocess.Popen("echo \" if ( \$request_uri ~* '^"+str(oringUrl)+"/?$' ) { rewrite . "+str(destinyUrl)+" permanent; } \" >> rules-formatted.inc", shell=True)
         return
-    else:
-        print ("FAIL HTML!!!")
-
     #Formats the rules in the default value.
-    subprocess.Popen("echo \"rewrite '^/"+str(oringUrl)+"/?$' "+str(destinyUrl)+" permanent;\" >> rules-formatted.inc", shell=True)
+    subprocess.Popen("echo \"rewrite '^"+str(oringUrl)+"/?$' "+str(destinyUrl)+" permanent;\" >> rules-formatted.inc", shell=True)
     pass
 
 def convertCSV(fileToFormat):
@@ -46,27 +48,46 @@ def convertCSV(fileToFormat):
             fformat(line[0],line[1])
     #Test added only for testing porpuses.
     subprocess.Popen("echo \"------ New Test -----\" >> rules-formatted.inc", shell=True)
+    print ("CSV file formatted")
+    print ("The rules were generated in the file: rules-formatted.inc")
+    pass
+
+def convertTXT(fileToFormat):
+    with open(str(fileToFormat)) as f:
+        lines = f.read().split("\n")
+        for l in lines:
+            urls = str(l).split(",")
+            #Remove the blank spaces from the urls before any process
+            urls[0] = urls[0].strip()
+            urls[1] = urls[1].strip()
+            fformat(urls[0],urls[1])
+    subprocess.Popen("echo \"------ New Test -----\" >> rules-formatted.inc", shell=True)
+    print ("TXT file formatted")
     print ("The rules generated in the file: rules-formatted.inc")
     pass
 
-
-#Body of our script
+#Body of the script
 #### ---------------------------------------#####
 #################################################
-fileToFormat = sys.argv[1]
-#Validation for no files passed as parameters in the Script
-if len(sys.argv) != 2:
+try:
+    fileToFormat = sys.argv[1]
+except IndexError:
     print ("Please include the file of the rules to be formatted")
     exit()
-elif str(fileToFormat).endswith('.csv'):
-    print ("Formatting the CSV file..."+fileToFormat)
-    convertCSV(fileToFormat)
-    exit()
-#elif str(len(sys.argv[1])).endswith('.txt'):
-#    print ("Formatting the TXT file...")
-    ##NEXT FEATURE
-    #convertTXT(len(sys.argv))
-#    exit()
-#else:
-#    print ("The extension of the file is not supported")
-#    exit()
+##################################################################
+#validation of the extension of the parameter file so it can decide how to proceed
+#the lower function() will leave avoid any issues with the validation of the extension of the file
+#for example: m.lower().endswith(('.png', '.jpg', '.jpeg'))
+else:
+    if str(fileToFormat).lower().endswith('.csv'):
+        print ("Formatting the CSV file..."+fileToFormat)
+        convertCSV(fileToFormat)
+        exit()
+    elif str(fileToFormat).lower().endswith('.txt'):
+        print ("Formatting the TXT file...")
+        ##NEXT FEATURE
+        convertTXT(str(fileToFormat))
+        exit()
+    else:
+        print ("The extension of the file is not supported")
+        exit()
